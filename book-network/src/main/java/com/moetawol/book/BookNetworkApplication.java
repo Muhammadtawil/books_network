5791100f4abfe718cdd5667d0b9faa2682270535
Muhammadtawil <muhammad.tawil@hotmail.com>
2025-04-11 08:41:34 +0300

    add auth and security Implementation

--- book-network/src/main/java/com/moetawol/book/BookNetworkApplication.java
+++ book-network/src/main/java/com/moetawol/book/BookNetworkApplication.java
@@ -1,13 +1,25 @@
 package com.moetawol.book;
 
+import com.moetawol.book.role.Role;
+import com.moetawol.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+//@EnabledJpaAuditing
 public class BookNetworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookNetworkApplication.class, args);
 	}
-
+@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
